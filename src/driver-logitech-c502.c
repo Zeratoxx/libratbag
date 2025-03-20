7e56fa7258a6283fc71bab488cb4072a1e9e4855
Zeratoxx <35294329+Zeratoxx@users.noreply.github.com>
2025-03-20 01:25:36 +0100

    Create driver-logitech-c502.c
    feat(g502-g-shift): create new driver file since g-shift is a virtual approach.

---
+++ src/driver-logitech-c502.c
@@ -0,0 +1,8 @@
+/** 
+*
+* driver for g502 mice since they have a G-Shift button. 
+* Issue is that the actual hardware driver for G502 mice only knows the plain buttons as they are only physical switches. 
+* Thus, the idea of G-Shift with its second layer for buttons is only virtual and because of this, we need a new driver file here at this point.
+*
+* WIP as of 20th of March 2025
+*/
